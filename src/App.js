667ffd4bb9406d9ee5e93d01e142b64b9663b159
Xuanhieu <xuanhieu021001@gmail.com>
2023-05-23 21:28:53 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to Hieu.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React kkk
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
